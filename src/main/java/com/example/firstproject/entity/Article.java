8038413b6d498509c3f9e95ae4967245f4d29785
LEESEOKJAE <lsj6924@naver.com>
2025-07-21 02:23:37 +0900

    restAPI부터

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,9 +1,6 @@
 package com.example.firstproject.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,7 +15,7 @@ import lombok.ToString;
 public class Article {
 
     @Id
-    @GeneratedValue // 자동 생성
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 ID를 자동 생성 어노테이션
     private Long id;
 
     @Column
